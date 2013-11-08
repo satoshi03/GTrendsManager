a1b3245610fd88ad339b5e96c2c7e5020cc61d58
satoshi03 <innamisatoshi@gmail.com>
2013-11-08 21:11:38 +0900

    Update example.rb
    Added example of getResultAsArray method

--- example.rb
+++ example.rb
@@ -1,6 +1,7 @@
 #! ruby
 
 require './GTrendManager'
+require 'pp'
 
 gm = GTrendManager.new()
 # search word is AMAZON, searched results are created from 2012/10 to 2012/11
@@ -17,3 +18,6 @@ gresult_2 = gm.request(GRequest.new("AMAZON+Kindle", 2012, 10, 3))
 
 # output search result as csv file
 gm.outputAsCsv(gresult_2, "result_amazon_and_kindle.csv")
+
+# convert search result to array(hash) and print it : Result is [ {"date"=>"2013-10-1", "number"=>"83"}, ... ]
+pp gm.getResultAsArray(gresult_2)
